a7b913600145f92f51fa4187bd0f36bccc265cda
andy_pl <andrzejw.baranowski@gmail.com>
2018-01-25 22:39:44 +0100

    Added Methods for score buttons

--- app/src/main/java/com/example/android/courtconter/MainActivity.java
+++ app/src/main/java/com/example/android/courtconter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.courtconter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +19,32 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Adds 3 points
+     */
+    public void plus_3(View view) {
+        int team_a_score = 0;
+        team_a_score = team_a_score + 3;
+        displayForTeamA(team_a_score);
+    }
+
+    /**
+     * Adds 2 points
+     */
+    public void plus_2(View view) {
+        int team_a_score = 0;
+        team_a_score = team_a_score + 2;
+        displayForTeamA(team_a_score);
+    }
+
+    /**
+     * Adds 1 points
+     */
+    public void plus_1(View view) {
+        int team_a_score = 0;
+        team_a_score = team_a_score + 1;
+        displayForTeamA(team_a_score);
     }
 }
